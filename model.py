968a494bf5f57f2db3f559248d241da95059df0d
Dennis <dennis.miczek@gmail.com>
2024-02-20 19:38:36 -0500

    refactor data loading, start notebook

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class ViTConfig:
     num_layers: int = 12
     mlp_dim: int = 3072
     dropout: float = 0.0
-    bias: bool = True
+    bias: bool = False
     n_embd: int = 768
 
 # replace eventually with nn.LayerNorm
@@ -32,17 +32,64 @@ class LayerNorm(nn.Module):
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        x = self.dropout(x)
+
+        return x
 
 
 class SelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.num_heads == 0
+
+        self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3, bias=config.bias)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.num_heads
+        self.n_embd = config.n_embd
+
+    def forward(self, x):
+        B, N, C = x.size()
+
+        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
+        q = q.view(B, N, self.n_head, C // self.n_head).transpose(1, 2)
+        k = k.view(B, N, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, N, self.n_head, C // self.n_head).transpose(1, 2)
+
+        # do NOT use causal attention as we are not dealing with sequential data (image patches are unordered)
+        y = torch.nn.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout=self.attn_dropout if self.training else 0, is_causal=False)
+
+        y = y.transpose(1, 2).contiguous().view(B, N, C)
+
+        y = self.resid_dropout(self.c_proj(y))
+        return y
         
 
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
+        self.ln = LayerNorm(config.n_embd, config.bias)
+        self.attn = SelfAttention(config)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        y = self.ln(x)
+        y = self.attn(y)
+        y = self.mlp(y)
+
+        x += y
+        return x
 
 
 class ViT(nn.Module):
@@ -50,7 +97,14 @@ class ViT(nn.Module):
         super().__init__()
         self.config = config
 
-        
+        self.transformer = nn.ModuleDict(dict(
+            token_encoding = nn.Embedding(config.in_channels, config.n_embd),
+            position_encoding = nn.Embedding(),
+            blocks = nn.ModuleList([Block(config) for _ in range(config.num_layers)]),
+            ln_post = LayerNorm(config.n_embd, config.bias)
+        ))
+
+         
 
     def forward(self, idx, targets=None):
         device = idx.device
--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 import os
 import numpy as np
 import torch
-from torch.utils.data import DataLoader, Dataset
 
-from model import ViT
+from model import ViTConfig, ViT
 
 dataset = 'cifar-10'
 block_size = 1
+device = 'cpu'
+device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 data_dir = os.path.join('data', dataset)
 train_images = np.memmap(os.path.join(data_dir, 'train-images.bin'), dtype=np.float32, mode='r')
@@ -14,6 +15,13 @@ train_labels = np.memmap(os.path.join(data_dir, 'train-labels.bin'), dtype=np.in
 test_images = np.memmap(os.path.join(data_dir, 'test-images.bin'), dtype=np.float32, mode='r')
 test_labels = np.memmap(os.path.join(data_dir, 'test-labels.bin'), dtype=np.int64, mode='r')
 
-def get_batch(images, labels, batch_size):
-    for i in range(0, len(images), batch_size):
-        yield images[i:i+batch_size], labels[i:i+batch_size]
+def get_batch(split, batch_size):
+    image_data, label_data = (train_images, train_labels) if split == 'train' else (test_images, test_labels)
+    ix = torch.randint(len(image_data) - block_size, (batch_size,))
+
+    x = torch.stack([torch.from_numpy(image_data[i:i+block_size].astype(np.float32)) for i in ix])
+    y = torch.tensor([torch.from_numpy(label_data[i:i+block_size].astype(np.int64)) for i in ix])
+
+    if device_type == 'cuda':
+        x = x.pin_memory().to(device, non_blocking=True)
+        y = y.pin_memory().to(device, non_blocking=True)
